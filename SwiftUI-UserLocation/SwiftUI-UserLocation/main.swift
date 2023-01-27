7312129435cfdca8dafe728828cb8b5a49cedc3a
CS Lab Account <cslab@olin304-03.mathcs.carleton.edu>
2023-01-26 19:32:46 -0600

    change inference

--- SwiftUI-UserLocation/SwiftUI-UserLocation/main.swift
+++ SwiftUI-UserLocation/SwiftUI-UserLocation/main.swift
@@ -10,17 +10,53 @@ import Foundation
 
 
 
-func readFile(){
+//func readFile(){
+//    errno = 0
+//    let path = "dataset.txt"
+//    if freopen(path, "r",stdin) == nil {
+//        perror(path)
+//    }
+//    while let line = readLine() {
+//        print(line)
+//    }
+//
+//}
+
+//create dictionary  Key -> Place , Val -> # of hours you're there
+//for loop - iterate through until 10:00 pm, add place to dict, if same
+//place in next line, increase counter in val, else add new place to dict
+//return biggest Key,Val pair
+
+func gethome(){
+    var home = Dictionary<String, Int>()
     errno = 0
     let path = "dataset.txt"
     if freopen(path, "r",stdin) == nil {
         perror(path)
     }
     while let line = readLine() {
-        print(line)
+        let location = line.components(separatedBy: ",")
+        let time: String = location[0]
+        let place: String = location[1]
+        let final_time = time.components(separatedBy: " ")
+        //The one we want
+        let clock: String = final_time[1]
+        
+        if ((Int(clock.prefix(2)) ?? 0 >= 22) || (Int(clock.prefix(2)) ?? 0 <= 6) ) {
+            var count = home[place] ?? 0
+            count = count + 1
+            home[place] = count
+        }
     }
-
+    //print(home.values.max() ?? 0)
+    let maxVal = home.values.max() ?? 0
+    let keys = home.filter { (k, v) -> Bool in v == maxVal}.map{ (k, v) -> String in k}
+    print("You most likely live in", keys[0])
+    
+    
+    
 }
 
 
-readFile()
+
+gethome()
