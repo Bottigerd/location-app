64f82f03c4565dea8a533a09cd4472ce572abea3
CS Lab Account <cslab@olin304-01.mathcs.carleton.edu>
2023-02-02 21:09:14 -0600

    [places-api] data now properly saved to structs

--- SwiftUI-UserLocation/SwiftUI-UserLocation/ContentViewModel.swift
+++ SwiftUI-UserLocation/SwiftUI-UserLocation/ContentViewModel.swift
@@ -8,7 +8,7 @@
 import MapKit
 
 enum MapDetails {
-    static let startingLocation = CLLocationCoordinate2D(latitude: 37.331516,                                                                              longitude: -121.891054)
+    static let startingLocation = CLLocationCoordinate2D(latitude: 44.460505,                                                                              longitude: -93.156647)
     static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
 }
 
@@ -86,152 +86,20 @@ struct ViewportStruct: Codable {
 
 /*
  MARK: - PlaceResponseStruct
-*/
+ */
 
 struct PlaceResponseStruct: Codable {
-    let result: PlaceResult?
-    let status: String?
-
+    let result: PlaceResult
+    let status: String
+    
     enum CodingKeys: String, CodingKey {
         case result, status
     }
 }
 
+// MARK: - Result
 struct PlaceResult: Codable {
-    let addressComponents: [AddressComponentStruct]
-    let adrAddress, businessStatus: String
-    let currentOpeningHours: CurrentOpeningHoursStruct
-    let formattedAddress, formattedPhoneNumber: String
-    let geometry: GeometryStruct
-    let icon: String
-    let iconBackgroundColor: String
-    let iconMaskBaseURI: String
-    let internationalPhoneNumber, name: String
-    let openingHours: OpeningHoursStruct
-    let photos: [PhotoStruct]
-    let placeID: String
-    let plusCode: PlusCodeStruct
-    let rating: Int
-    let reference: String
-    let reviews: [ReviewStruct]
-    let types: [String]
-    let url: String
-    let userRatingsTotal, utcOffset: Int
-    let vicinity: String
-    let website: String
-    let wheelchairAccessibleEntrance: Bool
-
-    enum CodingKeys: String, CodingKey {
-        case addressComponents = "address_components"
-        case adrAddress = "adr_address"
-        case businessStatus = "business_status"
-        case currentOpeningHours = "current_opening_hours"
-        case formattedAddress = "formatted_address"
-        case formattedPhoneNumber = "formatted_phone_number"
-        case geometry, icon
-        case iconBackgroundColor = "icon_background_color"
-        case iconMaskBaseURI = "icon_mask_base_uri"
-        case internationalPhoneNumber = "international_phone_number"
-        case name
-        case openingHours = "opening_hours"
-        case photos
-        case placeID = "place_id"
-        case plusCode = "plus_code"
-        case rating, reference, reviews, types, url
-        case userRatingsTotal = "user_ratings_total"
-        case utcOffset = "utc_offset"
-        case vicinity, website
-        case wheelchairAccessibleEntrance = "wheelchair_accessible_entrance"
-    }
-}
-
-struct CurrentOpeningHoursStruct: Codable {
-    let openNow: Bool
-    let periods: [CurrentOpeningHoursPeriodStruct]
-    let weekdayText: [String]
-
-    enum CodingKeys: String, CodingKey {
-        case openNow = "open_now"
-        case periods
-        case weekdayText = "weekday_text"
-    }
-}
-
-struct CurrentOpeningHoursPeriodStruct: Codable {
-    let close, periodOpen: PurpleCloseStruct
-
-    enum CodingKeys: String, CodingKey {
-        case close
-        case periodOpen = "open"
-    }
-}
-
-struct PurpleCloseStruct: Codable {
-    let date: String
-    let day: Int
-    let time: String
-}
-
-struct OpeningHoursStruct: Codable {
-    let openNow: Bool
-    let periods: [OpeningHoursPeriodStruct]
-    let weekdayText: [String]
-
-    enum CodingKeys: String, CodingKey {
-        case openNow = "open_now"
-        case periods
-        case weekdayText = "weekday_text"
-    }
-}
-
-struct OpeningHoursPeriodStruct: Codable {
-    let close, periodOpen: FluffyCloseStruct
-
-    enum CodingKeys: String, CodingKey {
-        case close
-        case periodOpen = "open"
-    }
-}
-
-struct FluffyCloseStruct: Codable {
-    let day: Int
-    let time: String
-}
-
-struct PhotoStruct: Codable {
-    let height: Int
-    let htmlAttributions: [String]
-    let photoReference: String
-    let width: Int
-
-    enum CodingKeys: String, CodingKey {
-        case height
-        case htmlAttributions = "html_attributions"
-        case photoReference = "photo_reference"
-        case width
-    }
-}
-
-struct ReviewStruct: Codable {
-    let authorName: String
-    let authorURL: String
-    let language, originalLanguage: String
-    let profilePhotoURL: String
-    let rating: Int
-    let relativeTimeDescription, text: String
-    let time: Int
-    let translated: Bool
-
-    enum CodingKeys: String, CodingKey {
-        case authorName = "author_name"
-        case authorURL = "author_url"
-        case language
-        case originalLanguage = "original_language"
-        case profilePhotoURL = "profile_photo_url"
-        case rating
-        case relativeTimeDescription = "relative_time_description"
-        case text, time, translated
-    }
+    let name: String
 }
 
 /*
@@ -271,6 +139,7 @@ final class ContentViewModel: NSObject, ObservableObject,
         }
     }
     
+    // MARK: - Check Location
     private func checkLocationAuthorization(){
         guard let locationManager = locationManager else { return }
         
@@ -294,17 +163,24 @@ final class ContentViewModel: NSObject, ObservableObject,
             // in case the next get location fails, save the current coordinates as the previous ones
             
             getReverseGeocode(coordinates: coordinates_string)
-            let place_id = reverse_geo_code_results?.results?[0].placeID ?? nil
-            
-            if (place_id != nil) {
-                getPlace(place_id: place_id!)
-                // force upwrapping because we should only go into the if statement if its not nil
-                address = place_results?.result?.name ?? reverse_geo_code_results?.results?[0].formattedAddress ?? "PendingLocation"
+            var place_id: String?
+            if (reverse_geo_code_results?.status == "OK") {
+                place_id = reverse_geo_code_results?.results?[0].placeID ?? nil
+                
+                if (place_id != nil) {
+                    getPlace(place_id: place_id!)
+                    if (place_results?.status == "OK"){
+                        // force upwrapping because we should only go into the if statement if its not nil
+                        
+                        address = place_results?.result.name ?? reverse_geo_code_results?.results?[0].formattedAddress ?? "PendingLocation"
+                    }
+                    
+                } else {
+                    address = reverse_geo_code_results?.results?[0].formattedAddress ?? "Pending Location"
+                }
+                
             }
-            else {
-                address = reverse_geo_code_results?.results?[0].formattedAddress ?? "Pending Location"
-            }
-            
+            print()
         @unknown default:
             break
         }
@@ -321,7 +197,7 @@ final class ContentViewModel: NSObject, ObservableObject,
     internal func getReverseGeocode(coordinates: String) {
         guard let url = URL(string: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coordinates + "&location_type=ROOFTOP&result_type=street_address&key=" + API_KEY)
         else{
-            print("ERROR: Malformed Request")
+            print("ERROR: Malformed Request (GET REVERSE GEOCODE)")
             return
         }
         
@@ -331,34 +207,36 @@ final class ContentViewModel: NSObject, ObservableObject,
             let decoder = JSONDecoder()
             if let data = data {
                 do {
-                    let reverse_geo_code_struct = try? decoder.decode(ReverseGeoCodingResponseStruct.self, from: data)
+                    let reverse_geo_code_struct = try decoder.decode(ReverseGeoCodingResponseStruct.self, from: data)
                     DispatchQueue.main.async {
-                    self.reverse_geo_code_results = reverse_geo_code_struct
+                        self.reverse_geo_code_results = reverse_geo_code_struct
                     }
                 } catch {
-                    print("ERROR: Could not decode JSON response")
+                    print("ERROR: Could not decode JSON response (GET REVERSE GEOCODE)")
                     return
                 }
             }
             
             print("REVERSE GEOCODING API CALL: " + coordinates)
+            /*
             // print JSON for testing purposes
             if let data = data, let string = String(data: data, encoding: .utf8){
                 print(string)
             }
+             */
             
-        }.resume()
+        }
+        task.resume()
     }
     
     // MARK: - Place
-    internal func getPlace(place_id: String) -> PlaceResponseStruct {
-        var place_struct = PlaceResponseStruct(result: nil, status: nil)
+    internal func getPlace(place_id: String) {
         
-        guard let url = URL(string: "  https://maps.googleapis.com/maps/api/place/details/json?place_id="
-                             + place_id + "&fields=name%2Crating%2Cformatted_phone_number&key=" + API_KEY)
+        guard let url = URL(string: "https://maps.googleapis.com/maps/api/place/details/json?place_id="
+                            + place_id + "&fields=name%2Crating%2Cformatted_phone_number&key=" + API_KEY)
         else{
-            print("ERROR: Malformed Request")
-            return place_struct
+            print("ERROR: Malformed Request (GET PLACE)")
+            return
         }
         
         
@@ -368,21 +246,26 @@ final class ContentViewModel: NSObject, ObservableObject,
             let decoder = JSONDecoder()
             if let data = data {
                 do {
-                    place_struct = try decoder.decode(PlaceResponseStruct.self, from: data)
+                    let place_struct = try decoder.decode(PlaceResponseStruct.self, from: data)
+                    DispatchQueue.main.async {
+                        self.place_results = place_struct
+                    }
                 } catch {
-                    print("ERROR: Could not decode JSON response")
+                    print("ERROR: Could not decode JSON response (GET PLACE)")
                 }
             }
             
             print("PLACE API CALL: " + place_id)
+            /*
             // print JSON for testing purposes
             if let data = data, let string = String(data: data, encoding: .utf8){
                 print(string)
             }
+             */
             
         }
         task.resume()
-        return place_struct
+        return
     }
     
 }
