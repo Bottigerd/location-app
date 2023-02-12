335639ab982da6822ef5d1421f10919249ed0597
CS Lab Account <cslab@olin302-03.mathcs.carleton.edu>
2023-02-11 18:07:28 -0600

    working home inference

--- SwiftUI-UserLocation/SwiftUI-UserLocation/InferenceView.swift
+++ SwiftUI-UserLocation/SwiftUI-UserLocation/InferenceView.swift
@@ -27,72 +27,48 @@ struct InferenceView: View {
     }
     
     
-    //here's some sample code on how to access the data from getAllLocationHistory()
-    //let data = getAllLocationHistory()
-    //for location in data {
-    //            var singleRow=[location.name!,location.latitude,location.altitude,location.longitude,location.time!] as [Any]
-    //            do xyz....
-    //        }
 
     private func gethome() -> String{
-        var home = Dictionary<String, Int>()
-        //storeData()
+        var home = Dictionary<String, Double>()
         let date = Date()
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         print(dateFormatter.string(from: date))
-        //end is today current time
-        //start is 4 days before today (10 pm)
+   
         let start = Calendar.current.date(byAdding: .day, value: -4, to: Date())!
         print(dateFormatter.string(from: start))
         
         let data = getAllLocationWithinRange(startTime: start, endTime: date)
-        for i in data{
-            let time = i.time
-            if let time = time {
-                let time_s = dateFormatter.string(from: time)
+
+        var i = 0
+        var originloc = data[0]
+        while(i != data.count-1){
+            i += 1
+            if (originloc.name != data[i].name){
+               
+                
+                let time_s = dateFormatter.string(from: originloc.time!)
                 let final_time = time_s.components(separatedBy: " ")
                 let hours = final_time[1]
-                
+
                 if ((Int(hours.prefix(2)) ?? 0 >= 22) || (Int(hours.prefix(2)) ?? 0 <= 8) ) {
-                    
+                    let interval = data[i].time?.timeIntervalSince(originloc.time!)
+                    home[originloc.name ?? "Invalid Location"] = interval
+                    originloc = data[i]
                 }
                 
+                
+                
             }
-            
-            
         }
+        let maxVal = home.values.max() ?? 0
+        let keys = home.filter { (k, v) -> Bool in v == maxVal}.map{ (k, v) -> String in k}
+     
         
-        //filter the data to only include 10pm-7am
-        
-        //loop through
-        //calculate time for each name change, add it to map (Name = key, TimeSpent = value)
-//        for i in 0...times.count{
-//
-//            //var hoursSpent = times[i+1].timeIntervalSince(times[i])
-//            //print(hoursSpent)
-//        }
-        
-        //go through map to find highest value -> that key is home
-        
-        let address = "HOME: Cassat"
+        let address = "You most likely live in " +  keys[0]
         return address
 
     }
-    
-//    private func storeData() {
-//        let data = getAllLocationHistory()
-//        for i in data{
-//            //print(i.name ?? "hi")
-//            let j = String(i.name ?? "hi")
-//            places.append(j)
-//            let formatter = DateFormatter()
-//            formatter.dateFormat = "yyyy/MM/dd HH:mm"
-//            let someDateTime = formatter.date(from: "2016/10/08 22:31")
-//            times.append((i.time ?? someDateTime)!)
-//        }
-//    }
-    
     
     
     // gets all location data from CoreData. Location data includes name,latitude,longitude,altitude and timestamp
@@ -120,7 +96,7 @@ struct InferenceView: View {
     
     private func getAllLocationWithinRange(startTime: Date, endTime: Date) -> [Location] {
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Location")
-        let sortOrder = NSSortDescriptor(key: "time", ascending: false)
+        let sortOrder = NSSortDescriptor(key: "time", ascending: true)
         fetchRequest.sortDescriptors = [sortOrder]
         fetchRequest.predicate = NSPredicate(format: "(time >= %@) AND (time <= %@)", startTime as CVarArg, endTime as CVarArg)
         let result = try! viewContext.fetch(fetchRequest) as! [Location]
