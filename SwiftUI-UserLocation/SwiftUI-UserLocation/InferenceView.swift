b155a29c48d5d89b67ab5e55b7720d71819db05b
CS Lab Account <cslab@olin304-02.mathcs.carleton.edu>
2023-02-19 16:34:06 -0600

    2 completed inferences

--- SwiftUI-UserLocation/SwiftUI-UserLocation/InferenceView.swift
+++ SwiftUI-UserLocation/SwiftUI-UserLocation/InferenceView.swift
@@ -7,6 +7,25 @@
 
 import SwiftUI
 import CoreData
+import Foundation
+
+//Credit to Stack Overflow: Vasily Bodnarchuk
+extension Date {
+
+    func fullDistance(from date: Date, resultIn component: Calendar.Component, calendar: Calendar = .current) -> Int? {
+        calendar.dateComponents([component], from: self, to: date).value(for: component)
+    }
+
+    func distance(from date: Date, only component: Calendar.Component, calendar: Calendar = .current) -> Int {
+        let days1 = calendar.component(component, from: self)
+        let days2 = calendar.component(component, from: date)
+        return days1 - days2
+    }
+
+    func hasSame(_ component: Calendar.Component, as date: Date) -> Bool {
+        distance(from: date, only: component) == 0
+    }
+}
 
 struct InferenceView: View {
     var places = [String]()
@@ -14,16 +33,11 @@ struct InferenceView: View {
     let viewContext = PersistenceController.shared.container.viewContext
 
     
-    
     var body: some View {
         VStack{
             Text("Information we know based on your location data: ")
             Text(gethome())
-            Button("test") {
-                test()}
-//
-//
-//            Text(getTop 5Locations())
+            Text(getTop5Locations())
         }
     }
     
@@ -45,8 +59,6 @@ struct InferenceView: View {
         }
         
         //loop through data
-        //NOTE: cases to handle: what if they get home before 10pm?
-        //also what if no data from next morning? - have a default end time
         var i = 0
         var originloc = data[0]
         while(i != data.count-1){
@@ -55,8 +67,9 @@ struct InferenceView: View {
                 let time_s = dateFormatter.string(from: originloc.time!)
                 let final_time = time_s.components(separatedBy: " ")
                 let hours = final_time[1]
-
-                if ((Int(hours.prefix(2)) ?? 0 >= 22) || (Int(hours.prefix(2)) ?? 0 <= 8) ) {
+                
+                
+                if ((Int(hours.prefix(2)) ?? 0 >= 22) || (Int(hours.prefix(2)) ?? 0 <= 8) || ((Int(hours.prefix(2)) ?? 0 < 22) && (data[i].time!.distance(from: originloc.time!, only: .day) == 1)) ) {
                     let interval = data[i].time?.timeIntervalSince(originloc.time!)
                     //check if key exists in map, add interval to curr value
                     if (home[originloc.name!] != nil)
@@ -83,19 +96,9 @@ struct InferenceView: View {
 
     }
     
-    //Cassat 5
-    //Olin 1
-    //Watson 3
-    //Anserson 4
-    //Sayles 7
-    //Leighton 9
-    //Libe 12
-    //[C,L,Li,A,S]
-    //[5,9,12,4,7]
     
-    //returns the 5 most frequented locations of user excluding their home
+    //returns the 5 most frequented locations of user
     private func getTop5Locations() -> String{
-        //IMPORTANT it doesn't delete API calls from data yet
         let data = getAllLocationCounts();
         print(data.count)
         if((data.count < 5) || (data[4].name == nil)){
@@ -110,19 +113,33 @@ struct InferenceView: View {
             
             for i in data{
                 if((i.name != nil) && (!topPlaces.contains(i.name!)) && (min! < i.count)) {
-                        //print(i.name!)
-                    var index = topCounts.firstIndex(of: min!)
+                    let index = topCounts.firstIndex(of: min!)
                     topCounts[index!] = i.count
                     topPlaces[index!] = i.name!
                     min = topCounts.min()
                 }
             }
             
-            let ret =  topPlaces.joined(separator: ",")
+            let top5 = [
+                topPlaces[0] : topCounts[0],
+                topPlaces[1] : topCounts[1],
+                topPlaces[2] : topCounts[2],
+                topPlaces[3] : topCounts[3],
+                topPlaces[4] : topCounts[4]
+            ]
+
+            let sortedVals = top5.sorted { $0.1 > $1.1 }
+            
+            var ret =  "Here Are Your Top 5 Places On Campus: \n"
+            var topcount = 1
+            for (place, count) in sortedVals{
+                print(topcount, place)
+                ret += String(topcount) + " " + place + "\n"
+                topcount += 1
+            }
             return ret
 
         }
-        return "hi"
     }
     
     
@@ -163,7 +180,7 @@ struct InferenceView: View {
         
     }
     
-    private func test(){
+   // private func test(){
 //        let date_1_str = "2022-03-20 10:15:30"
 //        let date_2_str = "2022-08-20 10:15:30"
 //        let addDateFormatter = DateFormatter()
@@ -171,8 +188,8 @@ struct InferenceView: View {
 //        let date_1=addDateFormatter.date(from: date_1_str) ?? Date()
 //        let date_2=addDateFormatter.date(from: date_2_str) ?? Date()
 //        getAllLocationWithinRange(startTime: date_1, endTime: date_2)
-        getAllLocationCounts()
-    }
+        //getAllLocationCounts()
+   // }
 //
 
     
